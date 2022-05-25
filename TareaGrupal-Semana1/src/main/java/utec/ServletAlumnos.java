4c086c44bf004d96e254a57252bbe6655a53418c
Juan Machin <juan.machin@estudiantes.utec.edu.uy>
2022-05-25 16:00:02 -0300

    hagan commits como la gente o la Mica nos reta

--- TareaGrupal-Semana1/src/main/java/utec/ServletAlumnos.java
+++ TareaGrupal-Semana1/src/main/java/utec/ServletAlumnos.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.utec.alumnos.BuscarAlumnos;
+import org.utec.alumnos.Alumno;
+
 /**
  * Servlet implementation class ServletAlumnos
  */
@@ -27,20 +30,29 @@ public class ServletAlumnos extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		BuscarAlumnos buscarAlumnos = new BuscarAlumnos();
+		Alumno alumno = new Alumno();
+		
 		String cedula = request.getParameter("cedula");
+		Integer ci = new Integer(cedula);
+		
 		String nombre = request.getParameter("nombre");
 		String apellido = request.getParameter("apellido");
 		
+		Alumno a1 = new Alumno();
 		
+		a1=buscarAlumnos.buscarAlumno(nombre, apellido, ci);
+		
+		System.out.println(a1.toString());
 		StringBuilder strBuilder = new StringBuilder();
 		
 		strBuilder.append("<h2> Resultado </h2>");
-		strBuilder.append("<p> El nombre es: " + nombre +  "</p>");
-		strBuilder.append("<p> El apellido es: " + apellido +  "</p>");
-		strBuilder.append("<p> La cedula es: " + cedula +  "</p>");
+		strBuilder.append("<p> El nombre es: " + a1.getNombre() +  "</p>");
+		strBuilder.append("<p> El apellido es: " + a1.getApellido() +  "</p>");
+		strBuilder.append("<p> La cedula es: " + a1.getCi() +  "</p>");
 		
 		String resp = strBuilder.toString();
-		System.out.println("hola");
+		System.out.println(a1);
 		response.setContentType("text/html");
 		// TODO Auto-generated method stub
 		
